802ffe6f9573faf885793d77c1d0fc2b62b5c552
madhubitvs <madhubitvs@gmail.com>
2022-01-24 20:59:28 +0000

    24jannew

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region  = "us-east-1"
-  shared_credentials_file = "root/.aws/credentials"
-  profile = "myprofile"
+  shared_credentials_file = "$HOME/.aws/credentials"
+  profile = "iam_myuser_profile"
 }
