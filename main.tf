61a40828687c33acf2c827add7d3ad513eb0a352
madhubitvs <madhubitvs@gmail.com>
2022-01-24 00:41:27 +0000

    Provider change

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
+/*
 #provider "aws" {
   #profile = "default"
  # region  = "us-east-1"
@@ -16,6 +17,7 @@ terraform {
   #secret_key = "X0tq+55aetQLP1GK2zum38vJQPBnUf26rxLI1UKp"
 
 #}
+*/
 
 
 resource "aws_instance" "mat_first-instance" {
